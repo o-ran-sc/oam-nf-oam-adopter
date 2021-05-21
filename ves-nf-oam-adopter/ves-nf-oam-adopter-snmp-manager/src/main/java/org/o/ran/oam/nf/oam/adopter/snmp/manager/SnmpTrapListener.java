c89e96c9bc641248ecdb55f3a6cd4c693ffcb5e8
Claudio D. Gasparini <claudio.gasparini@intl.att.com>
2021-05-21 10:40:39 +0200

    Fix Sonar complains
    Issue-ID: OAM-215
    Signed-off-by: Claudio D. Gasparini <claudio.gasparini@intl.att.com>
    Change-Id: I48fb824b105a02ebf049907bb759de6c7d33693a

--- ves-nf-oam-adopter/ves-nf-oam-adopter-snmp-manager/src/main/java/org/o/ran/oam/nf/oam/adopter/snmp/manager/SnmpTrapListener.java
+++ ves-nf-oam-adopter/ves-nf-oam-adopter-snmp-manager/src/main/java/org/o/ran/oam/nf/oam/adopter/snmp/manager/SnmpTrapListener.java
@@ -19,22 +19,15 @@
 
 package org.o.ran.oam.nf.oam.adopter.snmp.manager;
 
-import com.google.gson.Gson;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Optional;
 import org.o.ran.oam.nf.oam.adopter.api.VesEventNotifier;
 import org.o.ran.oam.nf.oam.adopter.snmp.manager.api.TimeZoneOffsetService;
 import org.o.ran.oam.nf.oam.adopter.snmp.manager.mapper.SnmpMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.snmp4j.CommandResponder;
-import org.snmp4j.CommandResponderEvent;
 import org.snmp4j.MessageDispatcher;
 import org.snmp4j.MessageDispatcherImpl;
-import org.snmp4j.PDU;
 import org.snmp4j.Snmp;
 import org.snmp4j.mp.MPv2c;
 import org.snmp4j.smi.UdpAddress;
@@ -42,23 +35,18 @@ import org.snmp4j.transport.DefaultUdpTransportMapping;
 import org.snmp4j.util.MultiThreadedMessageDispatcher;
 import org.snmp4j.util.ThreadPool;
 
-final class SnmpTrapListener implements CommandResponder, Runnable {
+final class SnmpTrapListener implements Runnable {
 
     private static final Logger LOG = LoggerFactory.getLogger(SnmpTrapListener.class);
     private static final int THREADS_SIZE = 2;
     private final String hostPortAddress;
-    private final SnmpMapper mapper;
-    private final VesEventNotifier vesEventNotifier;
-    private final Gson gson = new Gson();
-    private final TimeZoneOffsetService timeZoneOffsetService;
+    private final SNMPCommandResponder commandResponder;
 
     public SnmpTrapListener(final String host, final Integer port, final SnmpMapper mapper,
             final VesEventNotifier vesEventNotifier, final TimeZoneOffsetService timeZoneOffsetService) {
         LOG.info("SnmpTrapListener listening on {}:{}", host, port);
         this.hostPortAddress = host + "/" + port;
-        this.mapper = mapper;
-        this.vesEventNotifier = vesEventNotifier;
-        this.timeZoneOffsetService = timeZoneOffsetService;
+        this.commandResponder = new SNMPCommandResponder(timeZoneOffsetService, mapper, vesEventNotifier);
     }
 
     @Override
@@ -79,38 +67,12 @@ final class SnmpTrapListener implements CommandResponder, Runnable {
     private synchronized void listenSnmp(final MessageDispatcher dispatcher,
             final DefaultUdpTransportMapping snmpTarget) {
         try (final Snmp snmp = new Snmp(dispatcher, snmpTarget)) {
-            snmp.addCommandResponder(this);
+            snmp.addCommandResponder(commandResponder);
             snmpTarget.listen();
             LOG.debug("Listening on {}", snmpTarget);
             wait();
         } catch (final InterruptedException | IOException ex) {
             Thread.currentThread().interrupt();
         }
-    }
-
-    /**
-     * This method will be called whenever a pdu is received on the given port
-     * specified in the listen() method.
-     */
-    @Override
-    public synchronized void processPdu(final CommandResponderEvent cmdRespEvent) {
-        LOG.info("Received PDU");
-        final PDU pdu = cmdRespEvent.getPDU();
-        if (pdu == null) {
-            LOG.warn("Ignoring PDU.");
-            return;
-        }
-
-        final UdpAddress address = (UdpAddress) cmdRespEvent.getPeerAddress();
-        final ZoneId optZoneId = timeZoneOffsetService.getTimeZone(address.getInetAddress().getHostAddress());
-        final String timeZone = Optional.ofNullable(optZoneId)
-            .map(zoneId -> "UTC" + LocalDateTime.now().atZone(zoneId).getOffset().toString()).orElse(null);
-
-        mapper.toEvent(address, timeZone, pdu)
-                .flatMapCompletable(vesEventNotifier::notifyEvents)
-                .doOnSubscribe(result -> LOG.debug("SNMP Trap processing started"))
-                .doOnComplete(() -> LOG.debug("SNMP Trap processed successfully"))
-                .doOnError(error -> LOG.error("Failed to process SNMP Trap", error))
-                .subscribe();
     }
 }
